23de28d29fdc8e82344a086b34d78521c0403472
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-10-19 23:43:06 +0530

    Changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude=tests * travis@68.183.94.49:/home/nxtdoordeals/api
+  rsync -a --exclude={"tests","deploy.sh","travis_rsa.enc"} * travis@68.183.94.49:/home/nxtdoordeals/api
   echo "Deployed successfully!"
 else
   echo "Not deploying, since the branch isn't main."
 fi
+
+ssh travis@68.183.94.49 'systemctl restart nginx'
