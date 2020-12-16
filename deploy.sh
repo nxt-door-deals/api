1e72aba9074492bbd414be63abb7b9cf4e0b8f67
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-12-16 19:55:23 +0530

    Excluded alembic related files in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={"tests","deploy.sh","travis_rsa.enc"} * travis@68.183.94.49:/home/nxtdoordeals/api
+  rsync -a --exclude={"tests","deploy.sh","travis_rsa.enc", "alembic", "alembic.ini", "isort.cfg"} * travis@68.183.94.49:/home/nxtdoordeals/api
   echo "Deployed successfully!"
 else
   echo "Not deploying, since the branch isn't main."
