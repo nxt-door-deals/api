e9a06d4fecc76b4bfc78afda6a3136848b97bb9f
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-02-23 13:42:34 +0530

    Changes to the websocket connect feature

--- routers/websocket_server.py
+++ routers/websocket_server.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 from typing import List
 
 from better_profanity import profanity
@@ -15,21 +16,70 @@ from database.models import ChatHistory
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections = []
 
-    async def connect(self, websocket: WebSocket):
+    def create_connection_object(
+        self,
+        websocket: WebSocket,
+        chat_id: str,
+        private_chat_list: Dict,
+        private_connections: List,
+    ):
+        private_connections.append(websocket)
+        private_chat_list["chat_id"] = chat_id
+        private_chat_list["private_connection_list"] = private_connections
+        self.active_connections.append(private_chat_list.copy())
+
+    async def connect(self, websocket: WebSocket, chat_id: str):
+        """Creates the list of web socket connections. The list of dictonaries, will have
+        the chat id and list of private connections as keys.
+
+        private_chat_list = {
+            "chat_id": "chat_id string",
+            "private_connection_list":
+                    [websocket_1, websocket_2] # always two connections per  chat id
+        }
+        """
+        private_connections = []
+
+        private_chat_list = {}
         await websocket.accept()
-        self.active_connections.append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        if len(self.active_connections) != 0:
+            for connection in self.active_connections:
+                if connection["chat_id"] == chat_id:
+                    private_connections = connection["private_connection_list"]
+                    private_connections.append(websocket)
+                    connection["private_connection_list"] = private_connections
+                    break
+            else:
+                self.create_connection_object(
+                    websocket, chat_id, private_chat_list, private_connections
+                )
+        else:
+            self.create_connection_object(
+                websocket, chat_id, private_chat_list, private_connections
+            )
+
+        # self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket, chat_id: str):
+        if len(self.active_connections) != 0:
+            for connection in self.active_connections:
+                if connection["chat_id"] == chat_id:
+                    connection["private_connection_list"].remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+    async def broadcast(self, message: str, chat_id: str):
+        if len(self.active_connections) != 0:
+            for connection in self.active_connections:
+                if connection["chat_id"] == chat_id:
+                    for one_to_one_connection in connection[
+                        "private_connection_list"
+                    ]:
+                        await one_to_one_connection.send_text(message)
 
 
 manager = ConnectionManager()
@@ -76,28 +126,30 @@ def save_chat_message(data: str, chat_id: str, db: Session):
 
 
 # End points
-@router.websocket("/ws")
+@router.websocket("/ws/")
 async def default_websocket(
-    websocket: WebSocket, db: Session = Depends(get_db)
+    websocket: WebSocket,
+    chat_id: str,
+    client_id: int,
+    db: Session = Depends(get_db),
 ):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            data_json = json.loads(data)
-            data_json["data"] = profanity.censor(data_json["data"])
 
-            chat_id = data_json["chat_id"]
-            client_id = data_json["client_id"]
+    clients = get_valid_client_list(chat_id, db)
 
-            clients = get_valid_client_list(chat_id, db)
+    if client_id in clients:
 
-            if client_id in clients:
+        await manager.connect(websocket, chat_id)
+
+        try:
+            while True:
+                data = await websocket.receive_text()
+                data_json = json.loads(data)
+                data_json["data"] = profanity.censor(data_json["data"])
                 data = json.dumps(data_json)
                 save_chat_message(data, chat_id, db)
-                await manager.broadcast(data)
-            else:
-                await handle_rejected_connections(websocket)
+                await manager.broadcast(data, chat_id)
 
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
+        except WebSocketDisconnect:
+            manager.disconnect(websocket, chat_id)
+    else:
+        await handle_rejected_connections(websocket)
